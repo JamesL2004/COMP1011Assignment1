be9ffd57e05e0793aa983ca71f1898b7292cdc09
JamesL2004 <James002>
2023-10-11 00:05:12 -0400

    Upgraded the styling on the chart view and added the button function to switch between views.

--- src/main/java/com/example/comp1011assingment1/ChartViewController.java
+++ src/main/java/com/example/comp1011assingment1/ChartViewController.java
@@ -1,4 +1,28 @@
 package com.example.comp1011assingment1;
 
-public class ChartViewController {
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.chart.ScatterChart;
+
+import java.io.IOException;
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class ChartViewController implements Initializable {
+
+    @FXML
+    private ScatterChart<?, ?> scatterChart;
+
+    @FXML
+    void changeToTableView(ActionEvent event) throws IOException {
+        SceneChanger.changeScenes(event, "table-view.fxml" );
+    }
+
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle) {
+
+    }
 }
+
