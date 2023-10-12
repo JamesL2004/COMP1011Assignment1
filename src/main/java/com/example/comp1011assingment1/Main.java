2c0d63f3f59d96a0c02715d4ad316f19d7c11f1a
JamesL2004 <James002>
2023-10-11 20:10:19 -0400

    Added icon to the top bar, and changed the background image on the chart view to a gradient.

--- src/main/java/com/example/comp1011assingment1/Main.java
+++ src/main/java/com/example/comp1011assingment1/Main.java
@@ -3,6 +3,7 @@ package com.example.comp1011assingment1;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
+import javafx.scene.image.Image;
 import javafx.stage.Stage;
 
 import java.io.IOException;
@@ -10,9 +11,10 @@ import java.io.IOException;
 public class Main extends Application {
     @Override
     public void start(Stage stage) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(Main.class.getResource("table-view.fxml"));
+        FXMLLoader fxmlLoader = new FXMLLoader(Main.class.getResource("chart-view.fxml"));
         Scene scene = new Scene(fxmlLoader.load());
         stage.setTitle("James's Streaming Services Database");
+        stage.getIcons().add(new Image(Main.class.getResourceAsStream("images/icon.png")));
         stage.setScene(scene);
         stage.show();
     }
